02a4f1d1e677f4e58aba94fe4e7d9ea97235cbb6
24-f1001257 <harshkr918@gmail.com>
2025-05-14 18:29:42 +0530

    Almost done

---
+++ server.js
@@ -0,0 +1,161 @@
+const http = require('http');
+const url = require('url');
+const open = require('open');
+const querystring = require('querystring');
+const fs = require('fs');
+const path = require('path');
+
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+let userState = {};
+
+// Fill these from Google Console
+const CLIENT_ID = '1013708675568-1nav806sg6c94vvppsm82jfqod0iou26.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'http://localhost:3000/oauth2callback';
+
+const servePage = (res, pageName) => {
+    fs.readFile(path.join(__dirname, 'pages', pageName), (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading page');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+};
+
+const server = http.createServer(async (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+
+    if (pathname === '/') {
+        servePage(res, 'login.html');
+    }
+
+    else if (pathname === '/login') {
+        const authUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' + querystring.stringify({
+            client_id: CLIENT_ID,
+            redirect_uri: REDIRECT_URI,
+            response_type: 'code',
+            scope: 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets',
+            access_type: 'offline',
+            prompt: 'consent',
+        });
+        res.writeHead(302, { Location: authUrl });
+        res.end();
+    }
+
+    else if (pathname === '/oauth2callback') {
+        const code = parsedUrl.query.code;
+        const tokenRes = await fetch('https://oauth2.googleapis.com/token', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: querystring.stringify({
+                code,
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                redirect_uri: REDIRECT_URI,
+                grant_type: 'authorization_code',
+            }),
+        });
+
+        const tokenData = await tokenRes.json();
+        userState = {
+            accessToken: tokenData.access_token,
+            refreshToken: tokenData.refresh_token,
+        };
+
+        res.writeHead(302, { Location: '/select' });
+        res.end();
+    }
+
+    else if (pathname === '/select') {
+        if (!userState.accessToken) {
+            res.writeHead(302, { Location: '/' });
+            return res.end();
+        }
+        servePage(res, 'select.html');
+    }
+
+    else if (pathname === '/scanner') {
+        if (!userState.accessToken) {
+            res.writeHead(302, { Location: '/' });
+            return res.end();
+        }
+        servePage(res, 'scanner.html');
+    }
+
+    else if (pathname === '/spreadsheets') {
+        if (!userState.accessToken) {
+            res.writeHead(401);
+            return res.end('Not authorized');
+        }
+
+        const driveRes = await fetch('https://www.googleapis.com/drive/v3/files?q=mimeType="application/vnd.google-apps.spreadsheet"&fields=files(id,name)', {
+            headers: { Authorization: `Bearer ${userState.accessToken}` },
+        });
+
+        const driveData = await driveRes.json();
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(driveData));
+    }
+
+    else if (pathname === '/sheets') {
+        const spreadsheetId = parsedUrl.query.spreadsheetId;
+        if (!spreadsheetId || !userState.accessToken) {
+            res.writeHead(400);
+            return res.end('Missing spreadsheet ID or access token');
+        }
+
+        const sheetRes = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}`, {
+            headers: { Authorization: `Bearer ${userState.accessToken}` },
+        });
+
+        const sheetData = await sheetRes.json();
+        const sheets = (sheetData.sheets || []).map(s => ({
+            title: s.properties.title
+        }));
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ sheets }));
+    }
+    else if (parsedUrl.pathname === '/writeData') {
+        const { spreadsheetId, sheetName, data, customText } = parsedUrl.query;
+      
+        if (!spreadsheetId || !sheetName || !data || !customText || !userState.accessToken) {
+          res.writeHead(400);
+          return res.end('Missing parameters or access token');
+        }
+        const isoTrimmed = new Date().toISOString().split('.')[0] + 'Z';
+
+        // Make the request to Google Sheets API to append the data
+        const appendRes = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}!A1:A1:append?valueInputOption=RAW`, {
+          method: 'POST',
+          headers: { 
+            Authorization: `Bearer ${userState.accessToken}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            values: [[isoTrimmed, data, customText]], // Append both custom text and QR code data
+          }),
+        });
+      
+        if (appendRes.ok) {
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ success: true }));
+        } else {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ success: false }));
+        }
+      }
+      
+    else {
+        res.writeHead(404);
+        res.end('Not found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
